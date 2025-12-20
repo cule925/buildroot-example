1f586074996bc0444a11fbe838c20d0d1f7931cf
cule925 <iwanculumovic5555@gmail.com>
2025-12-20 15:10:10 +0100

    Add udev subsystem

--- ext/board/customboard/post-build.sh
+++ ext/board/customboard/post-build.sh
@@ -4,17 +4,6 @@
 set -u
 set -e
 
-BOARD_DIR="$(dirname $0)"
-DEFAULT_WPA_SUPPLICANT_CONF_FILE_PATH="${BOARD_DIR}/conf_wpa_supplicant/default_wpa_supplicant.conf"
-WPA_SUPPLICANT_CONF_FILE_PATH="${BOARD_DIR}/conf_wpa_supplicant/wpa_supplicant.conf"
-WPA_SUPPLICANT_CONF_TARGET_FILE_PATH="${TARGET_DIR}/etc/wpa_supplicant.conf"
-
-if [[ -e "$WPA_SUPPLICANT_CONF_FILE_PATH" ]]; then
-    cp $WPA_SUPPLICANT_CONF_FILE_PATH $WPA_SUPPLICANT_CONF_TARGET_FILE_PATH
-else
-    cp $DEFAULT_WPA_SUPPLICANT_CONF_FILE_PATH $WPA_SUPPLICANT_CONF_TARGET_FILE_PATH
-fi
-
 # Add a console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
